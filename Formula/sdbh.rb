73c2fbdbb6e888bc7185f93c1f8afff021e4ced7
Matthew Dickinson <matt+github@alpha345.com>
2026-02-03 18:40:53 -0700

    chore: bump sdbh to v1.0.2

--- Formula/sdbh.rb
+++ Formula/sdbh.rb
@@ -1,8 +1,8 @@
 class Sdbh < Formula
   desc "Shell DB History: store and query shell command history in SQLite"
   homepage "https://github.com/mgd43b/shelldbhist"
-  url "https://github.com/mgd43b/shelldbhist/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "9b5a8f1dc0adc74a42218c549e9be9e3b7644c9cef7819612f49d143c41969c4"
+  url "https://github.com/mgd43b/shelldbhist/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "50fb8445888854ff8762b1074f45f392d6acfdfad26084222b78021450e38c8e"
   license "MIT"
 
   depends_on "rust" => :build
